843537e5edf01db0adb680dc174e90e1fd3ab305
Miguel <miguelangelparrafranco@gmail.com>
2024-03-04 22:12:00 -0500

    Nuevo diseno

--- dotus-react/src/components/hero/Hero.js
+++ dotus-react/src/components/hero/Hero.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 import { JackInTheBox, Slide } from "react-awesome-reveal";
-import hero1 from '../../images/slider/1.jpg'
-import hero2 from '../../images/icon/brain.svg'
 import shape from '../../images/slider/line-1.png'
-
+import fondo from '../../images/backcomega.jpg'
 
 const Hero = () => {
 
@@ -12,8 +10,22 @@ const Hero = () => {
         window.scrollTo(10, 0);
     }
 
-    return (
+    const whiteSectionStyle = {
+        height: '100%',
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        width: '60%',
+        zIndex: -1,
+        clipPath: 'polygon(50% 0, 100% 0, 100% 100%, 0% 100%)',
+        backgroundImage: `url(${fondo})`,
+        backgroundSize: '80% 80%',
+        backgroundPosition: '110% 99%',
+    };
 
+
+
+    return (
         <section className="static-hero" id='div'>
             <div className="hero-container">
                 <div className="hero-inner">
@@ -21,29 +33,24 @@ const Hero = () => {
                         <div className="row align-items-center">
                             <div className="col-xl-7 col-lg-8 col-12">
                                 <div className="wpo-static-hero-inner">
-                                    <div className="slide-title">
-                                        <Slide direction="up" duration="800" triggerOnce="true">
-                                            <Link onClick={ClickHandler} to="/services" className="slide-title-btn"> <span></span> 24/07 Emergency Service</Link>
-                                        </Slide>
-                                    </div>
                                     <div className="slide-sub-title">
-                                        <Slide direction="up" duration="1000" triggerOnce="true">
-                                            <h2>Doctor always ready for service.</h2>
+                                        <Slide direction="up" duration="1000" triggerOnce={true}>
+                                            <h2>Un slogan médico aquí.</h2>
                                         </Slide>
                                     </div>
                                     <div className="slide-text">
-                                        <Slide direction="up" duration="1500" triggerOnce="true">
+                                        <Slide direction="up" duration="1500" triggerOnce={true}>
                                             <ul>
-                                                <li>Prescriptions & treatment plans.</li>
-                                                <li>Always available for emergency service.</li>
-                                                <li>Low visit and even less with insurance.</li>
+                                                <li>Cotiza tus consultas médicas.</li>
+                                                <li>Visualiza tus exámenes médicos.</li>
+                                                <li>Agenda citas con nuestros expertos.</li>
                                             </ul>
                                         </Slide>
                                     </div>
                                     <div className="clearfix"></div>
                                     <div className="slide-btn">
-                                        <Slide direction="up" duration="1600" triggerOnce="true">
-                                            <Link onClick={ClickHandler} className="theme-btn" to="/appoinment">Online Appoinment</Link>
+                                        <Slide direction="up" duration="1600" triggerOnce={true}>
+                                            <Link onClick={ClickHandler} className="theme-btn" to="/appoinment">Agenda Ahora</Link>
                                         </Slide>
                                     </div>
                                 </div>
@@ -52,23 +59,10 @@ const Hero = () => {
                     </div>
                 </div>
             </div>
-            <div className="static-hero-right">
-                <div className="static-hero-img">
-                    <div className="static-hero-img-inner">
-                        <img src={hero1} alt="" />
-                        <div className="hero-img-inner-boder"></div>
-                    </div>
-                    <div className="icon-1">
-                        <div className="icon-img">
-                            <JackInTheBox direction="up" duration="2000" triggerOnce="true">
-                                <img src={hero2} alt="" />
-                            </JackInTheBox>
-                        </div>
-                        <span>Neurologist</span>
-                        <div className="icon-boder"></div>
-                    </div>
-                </div>
-            </div>
+
+            {/* Nueva sección blanca con línea diagonal */}
+            <div style={whiteSectionStyle}></div>
+
             <div className="shape-1">
                 <svg width="696" height="839" viewBox="0 0 696 839" fill="none">
                     <g opacity="0.6" filter="url(#filter0_f_221_39)">
